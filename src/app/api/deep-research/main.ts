af422dfac290761e6238b4eae0575fe4548c6f87
Ariel Blas <63609775+ArielBlas@users.noreply.github.com>
2025-05-09 23:43:35 -0300

    feat: exa ai integration

--- src/app/api/deep-research/main.ts
+++ src/app/api/deep-research/main.ts
@@ -1,4 +1,4 @@
-import { generateSearchQueries } from "./research-functions";
+import { generateSearchQueries, search } from "./research-functions";
 import { ResearchState } from "./types";
 
 export async function deepResearch(
@@ -6,6 +6,23 @@ export async function deepResearch(
   dataStream: any
 ) {
   const initialQueries = await generateSearchQueries(researchState);
+  let currentQueries = (initialQueries as any).searchQueries;
+
+  while (currentQueries && currentQueries.length > 0) {
+    const searchResults = currentQueries.map((query: string) =>
+      search(query, researchState)
+    );
+    const searchResultsResponses = await Promise.allSettled(searchResults);
+
+    const allSearchResults = searchResultsResponses
+      .filter(
+        (result) => result.status === "fulfilled" && result.value.length > 0
+      )
+      .map((result) => result.value)
+      .flat();
+
+    console.log("allSearchResults", allSearchResults);
+  }
 
   return initialQueries;
 }
--- src/app/api/deep-research/research-functions.ts
+++ src/app/api/deep-research/research-functions.ts
@@ -1,7 +1,8 @@
 import { z } from "zod";
-import { ResearchState } from "./types";
+import { ResearchState, SearchResult } from "./types";
 import { getPlanningPrompt, PLANNING_SYSTEM_PROMPT } from "./prompts";
 import { callModel } from "./model-caller";
+import { exa } from "./service";
 
 export async function generateSearchQueries(researchState: ResearchState) {
   const result = await callModel(
@@ -24,4 +25,42 @@ export async function generateSearchQueries(researchState: ResearchState) {
   );
 
   return result;
+}
+
+export async function search(
+  query: string,
+  ResearchState: ResearchState
+): Promise<SearchResult[]> {
+  try {
+    const searchResult = await exa.searchAndContents(query, {
+      type: "keyword",
+      numResults: 3,
+      startPublishedDate: new Date(
+        Date.now() - 365 * 24 * 60 * 60 * 1000
+      ).toISOString(), // 1 year ago
+      endPublishedDate: new Date().toISOString(),
+      startCrawlDate: new Date(
+        Date.now() - 365 * 24 * 60 * 60 * 1000
+      ).toISOString(), // 1 year ago
+      endCrawlDate: new Date().toISOString(),
+      excludeDomains: ["https://youtube.com"],
+      text: {
+        maxCharacters: 20000,
+      },
+    });
+
+    const filteredResults = searchResult.results
+      .filter((r) => r.title && r.text !== undefined)
+      .map((r) => ({
+        title: r.title || "",
+        url: r.url || "",
+        content: r.text || "",
+      }));
+
+    ResearchState.completedSteps++;
+
+    return filteredResults;
+  } catch (error) {
+    console.error("error: ", error);
+  }
 }
